9ef67ac74470ba0ae2ae8d26326d98447328372b
Constantin Roganov <rccbox@gmail.com>
2018-01-08 15:45:48 +0300

    [added] Python3 support [added] More SW1 as success mark after SELECT command [added] atr command [fixed] Behavior when exception without message attribute occur [changed] Reader selection behavior simplified in order to remove msvcrt dependency to achieve cross platform compatibility.

--- apducmd.py
+++ apducmd.py
@@ -1,8 +1,8 @@
 # -*- coding: UTF-8 -*-
-#-------------------------------------------------------------------------------
-# Copyright:  (c) Constantin Roganov, 2014
-# Licence:    <your licence>
-#-------------------------------------------------------------------------------
+# ------------------------------------------------------------------------------
+# Copyright © Constantin Roganov, 2014-2017
+# Licence:  MIT
+# ------------------------------------------------------------------------------
 
 """Command shell with ability to execute APDU just like commands and see
 SW and output data on the screen.
@@ -10,53 +10,24 @@ SW and output data on the screen.
 
 import sys
 import cmd
-import msvcrt
+import locale
 
 from smartcard.CardConnection import CardConnection, CardConnectionEvent
 from smartcard.Observer import Observable
 from smartcard.CardMonitoring import CardMonitor
 from smartcard.System import readers
 from smartcard.util import toBytes, toHexString, PACK
+from smartcard.Exceptions import CardConnectionException
 
 
 def select_reader():
-    """Select cardreader with console onscreen menu.
-    Returns selected reader object or None if no readers connected or user
-    decided to escape shell.
-    If system has a single reader connected it will be returned without of
-    menu drawing.
+    """Select the first of available readers.
+    Return smartcard.reader.Reader or None if no readers attached.
     """
     readers_list = readers()
 
-    if not readers_list:
-        return
-
-    if len(readers_list) == 1:
+    if readers_list:
         return readers_list[0]
-
-    else:
-        print 'Please choose reader number from list or 0 to exit:\n\n'
-
-        for i, r in enumerate(readers_list):
-            print '[{}] - {}\n'.format(i + 1, r)
-
-        print '\n[0] - Exit without choosing\n'
-
-        choice = None
-        while not msvcrt.kbhit():
-            choice = msvcrt.getche()
-
-            if not choice.isdigit():
-                continue
-
-            if choice == '0':
-                return
-
-            try:
-                return readers_list[int(choice) - 1]
-
-            except IndexError:
-                pass
 
 
 class APDUShell(cmd.Cmd):
@@ -65,29 +36,45 @@ class APDUShell(cmd.Cmd):
     Reader selection will be performed on shell start.
     Prompt will contain a selected reader's name and FID of latest selected EF or DF.
     Response data and SW for each APDU will be printed in console.
+
+    Additional commands are:
+        exit - exit shell
+        atr - print connected card's ATR as a hex string
     """
 
-    def __init__(self, *args, **kwargs):
-        # base classes are old style classes so super() not allowed here
-        cmd.Cmd.__init__(self, *args, **kwargs)
+    SELECT_COMMAND_INSTRUCTION = 0xA4
+    SELECT_SUCCESSFUL_SW1 = (0x90, 0x9E, 0x9F, 0x61)
+
+    intro = """Command shell to communicate with smart card using APDU.
+    Type APDU as a hex string.
+    Use 'exit' command to leave shell.
+    Use 'atr' command to print card's ATR.  
+    """
+
+    def __init__(self):
+        if sys.version_info.major == 2:
+            cmd.Cmd.__init__(self, completekey=None)
+        else:
+            super(APDUShell, self).__init__(completekey=None)
 
         self.reader = select_reader()
         self.card = None
         self.connection = None
         self.sel_obj = ''
+        self.atr = None
         self.card_monitor = CardMonitor()
         self.card_monitor.addObserver(self)
 
-    def _card_observer_update(self, observable, (addedcards, removedcards)):
+    def _card_observer_update(self, observable, handlers):
         """CardObserver interface implementation"""
 
+        addedcards, removedcards = handlers
+
         if self.card and self.card in removedcards:
-            self.sel_obj = ''
-            self.card = None
-            self.connection = None
+            self._clear_connection()
 
         for card in addedcards:
-            if card.reader == self.reader:
+            if str(card.reader) == str(self.reader):
                 self.card = card
                 self._set_up_connection()
                 break
@@ -96,16 +83,21 @@ class APDUShell(cmd.Cmd):
         """CardConnectionObserver interface implementation"""
 
         if 'connect' == event.type:
-            print 'Card inserted\n'
+            message = 'Card inserted'
 
         elif 'disconnect' == event.type:
-            print 'Card removed\n'
+            message = 'Card removed'
 
         elif 'command' == event.type:
-            print '> ', toHexString(event.args[0])
+            message = '> ' + toHexString(event.args[0])
 
         elif 'response' == event.type:
-            print '< ', '[{}]\n<  {:02X} {:02X}\n'.format(toHexString(event.args[0]), *event.args[1:])
+            message = '< [{}]\n<  {:02X} {:02X}'.format(toHexString(event.args[0]), *event.args[1:])
+
+        else:
+            message = 'Unknown event type: {}'.format(event.type)
+
+        print(message)
 
     def update(self, *args):
         """Dispatch a call between CardObserver and CardConnectionObserver interfaces
@@ -131,17 +123,30 @@ class APDUShell(cmd.Cmd):
             apdu = toBytes(line)
             data, sw1, sw2 = self.connection.transmit(apdu)
 
-            # if INS is A4 (SELECT) then catch and save FID
-            if apdu[1] != 0xA4:
+            # if INS is A4 (SELECT) then catch and save FID if select is successful
+            if apdu[1] != APDUShell.SELECT_COMMAND_INSTRUCTION or sw1 not in APDUShell.SELECT_SUCCESSFUL_SW1:
                 return
 
             self.sel_obj = toHexString(apdu[5:], PACK)
 
-        except TypeError as e:
-            print e.message
+        except (TypeError, CardConnectionException) as e:
+            try:
+                print(e.message.decode(locale.getpreferredencoding()))
+            except AttributeError:
+                print(e.__class__.__name__ + ' (no message given)')
 
-    def do_exit(self, arg):
+    def do_exit(self, _):
+        """React on 'exit' command"""
+        print('Bye!\n')
+
         return True
+
+    def do_atr(self, _):
+        """React on 'atr' command, print current card's ATR"""
+        if self.atr:
+            print(self.atr)
+        else:
+            print('Error: Card connection not found')
 
     def preloop(self):
         """Try to connect to cardreader.
@@ -149,6 +154,7 @@ class APDUShell(cmd.Cmd):
         """
 
         if self.reader is None:
+            print('\nError: Unable to connect to card reader\n')
             sys.exit()
 
         self.postcmd(None, None)
@@ -157,10 +163,10 @@ class APDUShell(cmd.Cmd):
         """Print the command prompt"""
 
         if self.card is None:
-            self.prompt = 'Please insert card into reader [{}]>'.format(self.reader)
+            self.prompt = 'Please insert card into reader [{}]> '.format(self.reader)
 
         else:
-            self.prompt = 'Using reader [{}]\n[{}] selected>'.format(self.reader, self.sel_obj or 'None')
+            self.prompt = 'Using reader [{}]\n[{}] selected> '.format(self.reader, self.sel_obj or 'None')
 
         return stop
 
@@ -170,8 +176,14 @@ class APDUShell(cmd.Cmd):
         self.connection = self.card.createConnection()
         self.connection.addObserver(self)
         self.connection.connect()
+        self.atr = toHexString(self.connection.getATR(), PACK)
+
+    def _clear_connection(self):
+        self.self_obj = ''
+        self.card = None
+        self.connection = None
+        self.atr = None
 
 
 if __name__ == '__main__':
-    apdu_sh = APDUShell()
-    apdu_sh.cmdloop()
+    APDUShell().cmdloop()
